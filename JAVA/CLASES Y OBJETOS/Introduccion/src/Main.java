f50a43a2f22c680b8fa9580663ea97826a7da19e
Zer0M4n <123420180+Zer0M4n@users.noreply.github.com>
2023-07-04 19:42:09 -0700

    new

---
+++ JAVA/CLASES Y OBJETOS/Introduccion/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public class  juan{
+        public int number = 45;
+        public juan(int number){
+            this.number = number;
+        }
+    }
+    public static void main(String[] args) {
+
+        int number = 45;
+        System.out.println("Hello world!");
+        juan j = new ;
+    }
+}
